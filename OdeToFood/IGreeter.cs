6ec72e983a0c7613b0d81a1209227ae2fe5bf6e9
Fellipe da Silva Vieira <fellipe.svieira@gmail.com>
2018-08-30 19:49:38 -0300

    Command-Line Tools and Code The ASPNET Core Project Structure Adding a Configuration Source Creating and Injecting Greeting Service

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration Configuration){
+            _configuration = Configuration;
+        }
+        
+        string IGreeter.GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
